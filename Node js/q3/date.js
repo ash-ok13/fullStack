64508f4f467c136ef6e20cef1b2777b6dcae4737
Ashok kumar <118625366+ash-ok13@users.noreply.github.com>
2023-07-04 16:39:45 +0530

    Delete date.js

--- Node js/q3/date.js
+++
@@ -1,7 +0,0 @@
-const dateTimeObject = new Date();
-module.exports.dateTimeVal = function(){
-    return dateTimeObject.toDateString() + dateTimeObject.toTimeString();
-}
-module.exports.timeVal = function(){
-    return dateTimeObject.toTimeString();
-}
